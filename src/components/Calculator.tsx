2ec25c6f6c9dd00f4441418ece0eea6e9da0a58f
Zach <zowen17@gmail.com>
2025-04-09 21:02:27 -0400

    linttest 2

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -25,7 +25,7 @@ export default function Calculator() {
         />
       </p>
       <button className="btn btn-primary" type="button" onClick={handleAddClick}> Add </button>
-      <p className="card-text">Total: {calcState.total}</p>
+      <p className="card-text">Total1: {calcState.total}</p>
       <p className="card-text">Previous Total1: {calcState.previousTotal}</p>
     </div>
   );
